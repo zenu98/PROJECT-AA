5bdc34c69013193e585b149f89f98615e2a2ec31
Seo, Heejeong <heejeong3394@gmail.com>
2023-04-06 12:35:00 +0900

    Backend 기본 설정

---
+++ Back/src/test/java/com/prjt_aa/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prjt_aa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
